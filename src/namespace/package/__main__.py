35d561ec67c92d43907b1f39df0243c4ff907769
Krystof Pesek <krystof.pesek@gmail.com>
2021-10-14 11:42:14 +0200

    init

--- src/namespace/package/__main__.py
+++ src/namespace/package/__main__.py
@@ -1,6 +1,13 @@
 # -*- cofding: utf-8 -*.
 
 from namespace.package import main
+import requests
+from bs4 import BeatifulSoup
+
+def main():
+    page = requests.get("https://irozhlas.cz/")
+    # soup = BeatifulSoup(page.content, 'html.parser')
+    print(page.text)
 
 if __name__ == "__main__":
     main()
