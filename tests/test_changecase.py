06ea3e443c39b625a87678ce017a67f7b617a3a7
David Landa <david.landa@protonmail.com>
2021-11-03 13:04:05 +0100

    Cleanup project

--- tests/test_changecase.py
+++
@@ -1,9 +0,0 @@
-from namespace.package import lowercased, uppercased
-
-
-def test_uppercased():
-    assert uppercased("uppercased") == "UPPERCASED"
-
-
-def test_lowercased():
-    assert lowercased("LOWERCASED") == "lowercased"
